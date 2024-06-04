6b901689c9d1879ca602d781aa04905851f2b960
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-06-04 15:36:27 -0400

    cc_6.js
    "Implement draw method for Ball class."

--- cc_6.js
+++ cc_6.js
@@ -8,4 +8,13 @@ class Ball {
         this.dy = dy;
         this.color = color;
     }
+
+    // Draw the ball on canvas
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
 }
