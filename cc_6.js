a60f43fcde0b8163fdf9f1bf27164d92150faaf5
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-06-04 15:33:41 -0400

    cc_6.js
    "Create Ball constructor with initial properties."

---
+++ cc_6.js
@@ -0,0 +1,11 @@
+// Ball constructor function
+class Ball {
+    constructor(x, y, radius, dx, dy, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
+    }
+}
